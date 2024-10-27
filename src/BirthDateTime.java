344feca8d31ba8abc528d0fa71705327737af4a9
scarlaos <scarlaos@mail.uc.edu>
2024-10-27 09:40:49 -0400

    Fixed work

--- src/BirthDateTime.java
+++ src/BirthDateTime.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class BirthDateTime
@@ -11,12 +12,35 @@ public class BirthDateTime
         int birthMinute = 0;
         int birthDay = 0;
 
-        birthYear = SafeInput.getRangedYearInt(in, "Enter your birth year", 1950,2015);
-        birthMonth = SafeInput.getRangedMonthInt(in, "Enter your birth month", 1,12);
-        birthDay = SafeInput.getRangedDayInt(in,"Enter you birth day, if program does not continue,backspace and retype a proper day for your month",1,31);
-        birthHour = SafeInput.getRangedHourInt(in,"Enter the hour you were born",1,24);
-        birthMinute = SafeInput.getRangedMinuteInt(in,"Enter the minute you were born",1,59);
+        birthYear = DevTest.getRangedInt(in,"What is your birth year?", 1950,2015);
+        birthMonth = DevTest.getRangedInt(in,"What is your birth month?",1,12);
 
-        System.out.println("You were born in " + birthYear + " on month " + birthMonth + ", day " + birthDay + ", hour " + birthHour + ", and minute " + birthMinute);
+
+        switch(birthMonth){
+            case 2:
+                birthDay = DevTest.getRangedInt(in, "What is your birthday?", 1,29);
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                birthDay = DevTest.getRangedInt(in, "What is your birthday?",1, 30);
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                birthDay = DevTest.getRangedInt(in,"What is your birthday?",1,31);
+                break;
+            default:
+                System.out.println("Invalid day: " + birthDay);
+                return;
+        }
+        birthHour = DevTest.getRangedInt(in,"What is your birth hour?",1,24);
+        birthMinute = DevTest.getRangedInt(in, "What is your birth minute?",1,59);
+        System.out.println("Your birthdate is: " + birthMonth + "/" + birthDay + "/" + birthYear + " at: " + birthHour + ":" + String.format("%02d", birthMinute));
     }
 }
